ea5a4b272d713678cc0a1b9a3f53159afa3a74b0
Olik1992 <ovlyakul30041992@gmail.com>
2020-02-09 12:50:56 +0200

    Updates

---
+++ precache-manifest.9b88e9de74664f2735e55b9e9dd2eed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4f23a5645b576f6a61175ec61e98e5",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "e4ade2b98367b38c0363",
+    "url": "/socialnetwork/static/css/main.68e6592c.chunk.css"
+  },
+  {
+    "revision": "74574a878bf43d7ef040",
+    "url": "/socialnetwork/static/js/2.c4c5b1dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/socialnetwork/static/js/2.c4c5b1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ade2b98367b38c0363",
+    "url": "/socialnetwork/static/js/main.4a89a856.chunk.js"
+  },
+  {
+    "revision": "0ccd922b28af2b4ceaa9",
+    "url": "/socialnetwork/static/js/runtime-main.a8e30ed1.js"
+  }
+]);
