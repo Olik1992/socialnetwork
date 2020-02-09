63e9c244d1aecb86b51c7f9d70a625bbbfc1fc88
Olik1992 <ovlyakul30041992@gmail.com>
2020-02-09 12:43:51 +0200

    first commit

---
+++ src/componets/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn0.iconfinder.com/data/icons/abstract-tree-wood-forest-ecology-landscape-eco-gr/1470/cmyk_printing_butterfly_logo_print-2-512.png' alt=""/>
+        </header>
+    );
+}
+
+export default Header;
